5759fb0a0a21afc32f30da30945e7509e4b4ec68
vferreirati <vferreirati@gmail.com>
2019-02-21 20:26:59 -0300

    layout QuizPage

--- lib/pages/quiz/quiz_bloc.dart
+++ lib/pages/quiz/quiz_bloc.dart
@@ -1,24 +1,52 @@
 import 'package:exata_questoes_app/models/api/questao_model.dart';
 import 'package:exata_questoes_app/models/filtro_model.dart';
 import 'package:exata_questoes_app/services/questao/questao_service.dart';
+import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 class QuizBloc {
   QuestaoService _questaoService;
   FiltroModel _filtro;
+  PageController pageController;
+  Map<int, bool> _questaoMap;
+  static const adInterval = 5;
 
   final _questoes = BehaviorSubject<List<QuestaoModel>>();
+  final _questoesRespondidasMap = BehaviorSubject<Map<int, bool>>();
 
   Stream<List<QuestaoModel>> get questoes => _questoes.stream;
+  Stream<Map<int, bool>> get questoesRespondidasMap =>
+      _questoesRespondidasMap.stream;
 
-  QuizBloc(this._questaoService);
-  
+  QuizBloc(this._questaoService) {
+    pageController = PageController();
+    _questaoMap = Map<int, bool>();
+  }
+
   void setup(List<QuestaoModel> initialQuestions, FiltroModel filtro) {
+    initialQuestions.forEach((questao) {
+      _questaoMap[questao.id] = false;
+    });
+    _questoesRespondidasMap.add(_questaoMap);
     _questoes.add(initialQuestions);
     this._filtro = filtro;
   }
-  
+
+  int calculatePageCount() {
+    final totalQuestions = _questoes.value.length;
+    final numberOfAds = (totalQuestions / adInterval).floor() - 1;
+    return totalQuestions;
+  }
+
+  void onPageChanged(int index) {}
+
+  void onAlternativaSelected(int questaoId) {
+    _questaoMap[questaoId] = true;
+    _questoesRespondidasMap.add(_questaoMap);
+  }
+
   void dispose() {
     _questoes.close();
+    _questoesRespondidasMap.close();
   }
 }
--- lib/pages/quiz/quiz_page.dart
+++ lib/pages/quiz/quiz_page.dart
@@ -40,18 +40,81 @@ class _QuizPageState extends State<QuizPage> {
         title: appBarTitle(_isHardcoreMode ? "Simulado" : "Questões"),
         centerTitle: true,
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.warning),
-            onPressed: _onNotifyBadQuestion
-          )
+          IconButton(icon: Icon(Icons.warning), onPressed: _onNotifyBadQuestion)
         ],
       ),
       body: Container(
-        child: Center(
-          child: Text("Questão será exibida aqui!"),
+        child: StreamBuilder<List<QuestaoModel>>(
+          initialData: _initialQuestions,
+          stream: _bloc.questoes,
+          builder: (context, snapshot) {
+            return PageView.builder(
+              controller: _bloc.pageController,
+              itemCount: _bloc.calculatePageCount(),
+              onPageChanged: _bloc.onPageChanged,
+              itemBuilder: (context, index) =>
+                  _buildPageItem(context, index, snapshot.data[index]),
+            );
+          },
         ),
       ),
     );
+  }
+
+  Widget _buildPageItem(BuildContext context, int index, QuestaoModel questao) {
+    return Container(
+      padding: EdgeInsets.only(top: 10, left: 8, right: 8),
+      child: Column(
+        children: <Widget>[
+          _buildEnunciado(questao),
+          SizedBox(height: 10),
+          Column(children: _buildAlternativas(questao))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEnunciado(QuestaoModel questao) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+        child: Text(
+          "[${questao.banca.nome} - ${questao.ano}] ${questao.enunciado}",
+          style: TextStyle(fontFamily: "MavenPro", fontSize: 12),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildAlternativas(QuestaoModel questao) {
+    return List<Widget>.generate(questao.alternativas.length, (index) {
+      final alternativa = questao.alternativas[index];
+      return Card(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+          child: StreamBuilder<Map<int, bool>>(
+            stream: _bloc.questoesRespondidasMap,
+            builder: (context, snapshot) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Radio(
+                    value: index,
+                    groupValue: snapshot.data[index]
+                        ? (alternativa.correta ? index : -1)
+                        : -1,
+                    onChanged: (alternativa) =>
+                        _bloc.onAlternativaSelected(questao.id),
+                  ),
+                  Text(alternativa.corpo,
+                      style: TextStyle(fontFamily: "MavenPro", fontSize: 12))
+                ],
+              );
+            },
+          ),
+        ),
+      );
+    });
   }
 
   void _onNotifyBadQuestion() {
